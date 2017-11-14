ddf9dd394c86e15f7c7cfd849296ea24f5c95344
miguelxmarquez <miguelxmarquez@gmail.com>
2017-11-13 22:44:52 -0400

    Fix Schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20171114011007) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "first_name"
+    t.string   "last_name"
     t.string   "image"
     t.string   "bio"
     t.datetime "created_at", null: false
