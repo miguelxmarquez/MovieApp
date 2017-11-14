4a59a7f678f0c18ff56ec7d84caad0a8bf43d2e4
miguelxmarquez <miguelxmarquez@gmail.com>
2017-11-13 21:52:15 -0400

    Adding Columns to Movie, Actor and Part Models

--- db/migrate/20171114010959_create_actors.rb
+++ db/migrate/20171114010959_create_actors.rb
@@ -1,7 +1,9 @@
 class CreateActors < ActiveRecord::Migration[5.0]
   def change
     create_table :actors do |t|
-
+      t.string :first_name
+      t.string :image
+      t.string :bio
       t.timestamps
     end
   end
